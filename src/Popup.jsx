32997fb6bb393c40893a44060353e03ec020fb0c
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-07-02 18:48:14 +0200

    first commit

---
+++ src/Popup.jsx
@@ -0,0 +1,12 @@
+import './index.css'
+
+const Popup = () => {
+  return (
+    <div>
+      <h1>Popup Page</h1>
+      <p>This is the content of the popup page.</p>
+    </div>
+  );
+};
+
+export default Popup;
