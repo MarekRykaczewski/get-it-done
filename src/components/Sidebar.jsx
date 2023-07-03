6e3eb2c69bf63a42b307ccec8559d90946cef932
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-07-03 13:16:23 +0200

    add Sidebar component

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,15 @@
+const Sidebar = () => {
+  return (
+    <nav className="flex flex-col bg-slate-200 h-full w-[300px]">
+        <h1 className='text-xl font-bold'> Get it done </h1>
+        <span> Test </span>
+        <span> Test </span>
+        <span> Test </span>
+        <span> Test </span>
+        <span> Test </span>
+        <span> Test </span>
+    </nav>
+  )
+}
+
+export default Sidebar
