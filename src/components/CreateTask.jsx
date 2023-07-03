e85ceb089d4fdc7d3a719fa6b6a712e7391df028
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-07-03 16:12:53 +0200

    add handleSubmit

--- src/components/CreateTask.jsx
+++ src/components/CreateTask.jsx
@@ -5,13 +5,25 @@ const CreateTask = ({ tasks, setTasks }) => {
   const [task, setTask] = useState({
     id: "",
     name: "",
-    status: "todo" // todo, in progress or done
+    done: true,
+    category: "do" // do, decide, delegate, delete
   })
 
-  console.log(task)
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    setTasks((prev) => {
+      const list = [...prev, task]
+
+      localStorage.setItem("tasks", JSON.stringify(list))
+
+      return list
+    })
+
+  }
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input 
       type="text" 
       className="border-2 border-slate-400 bg-slate-100 rounded-md mr-4 h-12 w-64 px-1"
