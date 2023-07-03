3c40222eab2a81b22d0c495918079643153725e2
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-07-03 13:17:23 +0200

    update App with new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import Matrix from './components/Matrix'
+import Sidebar from './components/Sidebar'
 import './index.css'
 
 function App() {
+
   return (
-    <>
-      <h1 className='text-3xl font-bold'> Test </h1>
-    </>
+    <div className='flex w-[100%] h-[100vh] items-center justify-between'>
+      <Sidebar />
+      <div className='flex flex-col mr-auto ml-auto gap-3 items-center justify-center'>
+        <Matrix />
+      </div>
+    </div>
   )
 }
 
