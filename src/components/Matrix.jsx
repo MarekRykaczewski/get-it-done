a7abd4ba803259b48e32625a0dd661b5714f3e6a
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-07-03 16:13:29 +0200

    render tasks in Matrix

--- src/components/Matrix.jsx
+++ src/components/Matrix.jsx
@@ -1,9 +1,31 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import CreateTask from "./CreateTask"
+import Task from "./Task"
 
 const Matrix = () => {
 
   const [tasks, setTasks] = useState([])
+
+  const [tasksDo, setTasksDo] = useState([])
+  const [tasksDecide, setTasksDecide] = useState([])
+  const [tasksDelegate, setTasksDelegate] = useState([])
+  const [tasksDelete, setTasksDelete] = useState([])
+
+  useEffect(() => {
+    setTasks(JSON.parse(localStorage.getItem("tasks")))
+  }, [])
+
+  useEffect(() => {
+    const fTasksDo = tasks.filter(task => task.category === "do")
+    const fTasksDecide = tasks.filter(task => task.category === "decide")
+    const fTasksDelegate = tasks.filter(task => task.category === "delegate")
+    const fTasksDelete = tasks.filter(task => task.category === "delete")
+  
+    setTasksDo(fTasksDo)
+    setTasksDecide(fTasksDecide)
+    setTasksDelegate(fTasksDelegate)
+    setTasksDelete(fTasksDelete)
+  }, [tasks])
 
   return (
     <div className="flex flex-col items-center gap-5">
@@ -14,16 +36,28 @@ const Matrix = () => {
       />
      <div className='grid auto-cols-min auto-rows-min px-10'>
                 
-        <div className='relative col-start-1 row-start-1 bg-green-200 w-[400px] h-[400px]'>
+        <div className='flex flex-col p-3 gap-2 items-center justify-center col-start-1 row-start-1 bg-green-200 w-[400px] h-[400px]'>
+          {tasksDo?.map(task => (
+            <Task key={task.id}> {task.name} </Task>
+          ))}
         </div>
 
         <div className='col-start-2 row-start-1 bg-blue-200 w-[400px] h-[400px]'>
+          {tasksDecide?.map(task => (
+            <div key={task.id}> {task.name} </div>
+          ))}
         </div>
 
         <div className='col-start-1 row-start-2 bg-orange-200 w-[400px] h-[400px]'>
+          {tasksDelegate?.map(task => (
+            <div key={task.id}> {task.name} </div>
+          ))}
         </div>
 
         <div className='col-start-2 row-start-2 bg-red-200 w-[400px] h-[400px]'>
+          {tasksDelete?.map(task => (
+            <div key={task.id}> {task.name} </div>
+          ))}
         </div>
 
     </div>
