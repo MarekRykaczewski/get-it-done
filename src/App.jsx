19a80f8b74991cde67f2235bc3ec78884b94a70d
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-07-05 14:51:49 +0200

    wrap in dnd context

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import Matrix from './components/Matrix'
 import Sidebar from './components/Sidebar'
 import './index.css'
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 
 function App() {
 
   return (
+    <DndProvider backend={HTML5Backend}>  
     <div className='flex w-[100%] h-[100vh] items-center justify-between'>
       <Sidebar />
       <div className='flex flex-col mr-auto ml-auto gap-3 items-center justify-center'>
         <Matrix />
       </div>
     </div>
+    </DndProvider> 
   )
 }
 
