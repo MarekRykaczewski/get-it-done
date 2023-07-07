508810e059e44f56a90f5c1e7a7a6eeac3a8dad4
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-07-07 20:37:19 +0200

    use CreateMatrix in Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,27 @@
+import { useState, useEffect } from "react"
+import CreateMatrix from "./CreateMatrix"
+
 const Sidebar = () => {
+
+  const [matrixes, setMatrixes] = useState([])
+
+  useEffect(() => {
+    setMatrixes(JSON.parse(localStorage.getItem("matrixes")) || [])
+  }, [])
+
   return (
     <nav className="flex flex-col bg-slate-200 h-full w-[300px]">
         <h1 className='text-xl font-bold'> Get it done </h1>
-        <span> Test </span>
-        <span> Test </span>
-        <span> Test </span>
-        <span> Test </span>
-        <span> Test </span>
-        <span> Test </span>
+        <CreateMatrix
+          matrixes={matrixes}
+          setMatrixes={setMatrixes}
+         />
+        {matrixes.map(matrix => (
+          <div key={matrix.id} id={matrix.id}> {matrix.name} </div>
+       ))}
     </nav>
   )
 }
 
 export default Sidebar
+
