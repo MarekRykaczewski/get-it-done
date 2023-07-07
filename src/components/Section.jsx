f984c39d540d880c44ce19dcf9379be34d03ced5
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-07-07 16:34:06 +0200

    Create Section componenet

---
+++ src/components/Section.jsx
@@ -0,0 +1,42 @@
+import Task from "./Task"
+import { useDrop } from "react-dnd"
+import { ItemTypes } from "./Task"
+
+
+const Section = ({ tasks, setTasks, color, position, category }) => {
+
+    const [{ isOver }, drop] = useDrop(() => ({
+        accept: ItemTypes.TASK,
+        drop: (item) => addItemToSection(item.id),
+        collect: (monitor) => ({
+          isOver: !!monitor.isOver()
+        })
+      }))
+    
+      const addItemToSection = (id) => {
+        console.log(category)
+        setTasks((prev) => {
+          const newTasks = prev.map(task => {
+            if (task.id === id) {
+              return {...task, category: category}
+            }
+    
+            return task
+          })
+    
+          localStorage.setItem("tasks", JSON.stringify(newTasks))
+    
+          return newTasks
+        })
+      }
+
+  return (
+    <div ref={drop} className={`flex flex-col p-3 gap-2 items-center justify-center ${position} ${color} w-[400px] h-[400px]`}>
+    {tasks.map(task => (
+      <Task key={task.id} id={task.id}> {task.name} </Task>
+    ))}
+  </div>
+  )
+}
+
+export default Section
