95c8b96558a1c4e1f3d56253a4c3b90f952af9d4
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-07-07 16:30:27 +0200

    update state logic and validate name length

--- src/components/CreateTask.jsx
+++ src/components/CreateTask.jsx
@@ -2,24 +2,33 @@ import { useState } from "react"
 import { v4 as uuidv4 } from 'uuid';
 
 const CreateTask = ({ tasks, setTasks }) => {
-  const [task, setTask] = useState({
+
+  const TASK_TEMPLATE = {
     id: "",
     name: "",
-    done: true,
+    done: false,
     category: "do" // do, decide, delegate, delete
-  })
+  }
+
+  const [task, setTask] = useState(TASK_TEMPLATE)
 
   const handleSubmit = (e) => {
     e.preventDefault()
 
-    setTasks((prev) => {
-      const list = [...prev, task]
+    if (task.name.length < 3) {
+      console.log("Task must be at least 3 characters long")
+    } else {
 
-      localStorage.setItem("tasks", JSON.stringify(list))
-
-      return list
-    })
-
+      setTasks((prev) => {
+        const list = [...prev, task]
+  
+        localStorage.setItem("tasks", JSON.stringify(list))
+  
+        return list
+      })
+  
+      setTask(TASK_TEMPLATE)
+    }
   }
 
   return (
@@ -28,7 +37,7 @@ const CreateTask = ({ tasks, setTasks }) => {
       type="text" 
       className="border-2 border-slate-400 bg-slate-100 rounded-md mr-4 h-12 w-64 px-1"
       value={task.name}
-      onChange={(e) => setTask({...task, id: uuidv4(), name: e.target.value})} 
+      onChange={(e) => setTask({...task, id: uuidv4(), name: e.target.value, createdAt: Date.now()})} 
       />
       <button className="bg-cyan-500 rounded-md px-4 h-12 text-white">
         Create
