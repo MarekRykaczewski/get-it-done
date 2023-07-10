56f6698b1a1034df077e76d1ea343db01792826d
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-07-10 19:33:22 +0200

    indentation

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -13,22 +13,22 @@ const Section = ({ currentMatrix, tasks, setTasks, color, position, category }) 
         })
       }))
     
-      const addItemToSection = (id) => {
-        console.log(category)
-        setTasks((prev) => {
-          const newTasks = prev.map(task => {
-            if (task.id === id) {
-              return {...task, category: category}
-            }
-    
-            return task
-          })
-    
-          localStorage.setItem(currentMatrix.id, JSON.stringify(newTasks))
-    
-          return newTasks
+    const addItemToSection = (id) => {
+      console.log(category)
+      setTasks((prev) => {
+        const newTasks = prev.map(task => {
+          if (task.id === id) {
+            return {...task, category: category}
+          }
+  
+          return task
         })
-      }
+  
+        localStorage.setItem(currentMatrix.id, JSON.stringify(newTasks))
+  
+        return newTasks
+      })
+    }
 
   return (
     <div ref={drop} className={`flex flex-col p-3 gap-2 items-center justify-center ${position} ${color} w-[400px] h-[400px]`}>
