0e4d3ff5d264888af1970280dc717394869d5ebb
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-07-13 18:42:28 +0200

    add open tab button to Popup.jsx

--- src/Popup.jsx
+++ src/Popup.jsx
@@ -1,10 +1,16 @@
 import './index.css'
 
 const Popup = () => {
+
+  const handleOpenTab = () => {
+    chrome.tabs.create({ url: chrome.runtime.getURL('index.html') });
+  };
+
   return (
     <div>
       <h1>Popup Page</h1>
       <p>This is the content of the popup page.</p>
+      <button onClick={handleOpenTab}>Open Tab</button>
     </div>
   );
 };
