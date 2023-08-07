c3f797c2ae2d198c04a32d294d295893c4d4c82a
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-08-07 15:38:34 +0200

    update handling matrix state

--- src/Popup.jsx
+++ src/Popup.jsx
@@ -11,7 +11,9 @@ const Popup = () => {
     const storedMatrixes = JSON.parse(localStorage.getItem('matrixes'));
     if (storedMatrixes) {
       setMatrixes(storedMatrixes);
-      setSelectedMatrix(storedMatrixes[0].id)
+      if (storedMatrixes.length > 0) {
+        setSelectedMatrix(storedMatrixes[0].id);
+      }
     }
   }, []);
 
@@ -46,9 +48,9 @@ const Popup = () => {
         <button className='bg-cyan-500 rounded-md px-4 py-1 text-white' onClick={handleOpenTab}>Open Matrixes</button>
       </div>
       
-      <form onSubmit={(e) => handleSubmit(e)} className='flex items-center flex-col gap-2'>
-        <div className='flex justify-around'>
-          <input placeholder='Add a new task' onChange={(e) => setNewTask(e.target.value)} className='border-2 border-slate-400 bg-slate-100 rounded-md mr-4 py-1 pl-2 px-1' type="text" />
+        {matrixes.length > 0 && <form onSubmit={(e) => handleSubmit(e)} className='flex items-center flex-col gap-2'>
+        <div className='flex gap-2 justify-around'>
+          <input placeholder='Add a new task' onChange={(e) => setNewTask(e.target.value)} className='border-2 w-full border-slate-400 bg-slate-100 rounded-md py-1 pl-2 px-1' type="text" />
           <select onChange={handleSelectChange}>
           {matrixes.map((matrix) => (
             <option key={matrix.id} value={matrix.id}>
@@ -58,7 +60,7 @@ const Popup = () => {
           </select>
         </div>
         <button className='w-full bg-cyan-500 rounded-md px-4 py-1 text-white'>Add to Matrix</button>
-      </form>
+      </form>}
     </div>
   );
 };
