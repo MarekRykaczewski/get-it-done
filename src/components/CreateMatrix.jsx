d25ea2ea141e938043f4160cd5ef11ba7745b4d0
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-08-09 21:28:49 +0200

    refactor CreateMatrix

--- src/components/CreateMatrix.jsx
+++ src/components/CreateMatrix.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react"
 import { v4 as uuidv4 } from 'uuid';
 
-const CreateMatrix = ({ setMatrixes }) => {
+const CreateMatrix = ({ matrixes, setMatrixes }) => {
 
   const MATRIX_TEMPLATE = {
-    id: "",
+    id: uuidv4(),
     name: "",
   }
 
@@ -15,19 +15,21 @@ const CreateMatrix = ({ setMatrixes }) => {
 
     if (matrix.name.length < 3) {
       console.log("Matrix name must be at least 3 characters long")
-    } else {
+      return
+    } 
 
-      setMatrixes((prev) => {
-        const list = [...prev, matrix]
-  
-        localStorage.setItem("matrixes", JSON.stringify(list))
-  
-        return list
-      })
-  
-      setMatrix(MATRIX_TEMPLATE)
-    }
+    const updatedMatrix = { ...matrix, id: uuidv4() };
+    const updatedMatrixes = [...matrixes, updatedMatrix];
+
+    setMatrixes(updatedMatrixes);
+    localStorage.setItem("matrixes", JSON.stringify(updatedMatrixes));
+
+    setMatrix(MATRIX_TEMPLATE)
   }
+
+  const handleMatrixNameChange = (e) => {
+    setMatrix({ ...matrix, name: e.target.value });
+  };
 
   return (
     <form className="flex self-center mb-10" onSubmit={handleSubmit}>
@@ -35,7 +37,7 @@ const CreateMatrix = ({ setMatrixes }) => {
       type="text" 
       className="border-2 border-slate-400 bg-slate-100 text-lg rounded-md pl-2 mr-4 h-12 w-64 px-1"
       value={matrix.name}
-      onChange={(e) => setMatrix({...matrix, id: uuidv4(), name: e.target.value})} 
+      onChange={handleMatrixNameChange} 
       placeholder="Create a new Matrix..."
       />
       <button className="bg-cyan-500 text-lg rounded-md px-4 h-12 text-white">
