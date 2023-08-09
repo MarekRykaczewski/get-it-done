49b130e85650122017af874bd25f5e83a8f8667c
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-08-09 21:07:06 +0200

    add guide modal

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react"
 import CreateMatrix from "./CreateMatrix"
+import Modal from "./Modal"
 
 const Sidebar = ({ setCurrentMatrix }) => {
 
   const [matrixes, setMatrixes] = useState([])
   const [editing, setEditing] = useState(false)
   const [title, setTitle] = useState("")
+  const [guideModal, setGuideModal] = useState(false)
 
   useEffect(() => {
     setMatrixes(JSON.parse(localStorage.getItem("matrixes")) || [])
@@ -44,7 +46,39 @@ const Sidebar = ({ setCurrentMatrix }) => {
 
   return (
     <nav className="relative flex flex-col px-20">
-        <h1 className='mt-10 text-4xl text-center font-logo text-white drop-shadow font-bold mb-10'> GET IT DONE 👔 </h1>
+        <div className="flex w-full justify-center items-center gap-3">
+          <h1 className='mt-10 text-4xl text-center font-logo text-white drop-shadow font-bold mb-10'> GET IT DONE 👔 </h1>
+          <button onClick={() => setGuideModal(true)} className='text-3xl text-slate-500 transition rounded-full p-2 hover:bg-slate-200'> 🛈 </button>
+          
+          <Modal open={guideModal}> 
+          <div className='flex flex-col gap-2'>
+            <h1 className='text-xl font-bold'> Use this extension to create Matrices and Tasks. </h1>
+            <p> Read more about Eisenhower Matrices <a className='text-blue-500 font-bold underline underline-offset-2 visited:text-purple-500' rel='noreferrer' target='_blank' href='https://en.wikipedia.org/wiki/Time_management#The_Eisenhower_Method'> here </a> </p>
+            <h1 className='text-xl font-bold text-center'> TL;DR </h1>
+            <p> Organize your Tasks based on the following categories: </p>
+            <div className='grid grid-rows-2 grid-cols-2 gap-3 max-w-[500px] max-h-[500px] aspect-square'>
+              <div className='bg-green-500 text-white rounded-lg p-6'> 
+                <h1 className='text-xl font-bold'> 1. Do first </h1>
+                <p> First focus on important tasks that need doing soon </p>
+              </div> 
+              <div className='bg-blue-500 text-white rounded-lg p-6'> 
+                <h1 className='text-xl font-bold'> 2. Schedule </h1>
+                <p> Important, but no-so-urgent </p>
+              </div> 
+              <div className='bg-orange-500 text-white rounded-lg p-6'> 
+                <h1 className='text-xl font-bold'> 3. Delegate </h1>
+                <p> Urgent, but less important </p>
+              </div> 
+              <div className='bg-red-500 text-white rounded-lg p-6'> 
+                <h1 className='text-xl font-bold'> 4. Delete </h1>
+                <p> Neither urgent nor important </p>
+              </div> 
+            </div>
+            <button onClick={() => setGuideModal(false)} className='absolute p-2 top-1 right-1'> ❌ </button>
+          </div>
+          </Modal>
+
+        </div>
         <CreateMatrix
           matrixes={matrixes}
           setMatrixes={setMatrixes}
