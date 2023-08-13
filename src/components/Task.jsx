0306f73b51a66a2ffba924874186aad42f28d589
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-08-13 21:06:47 +0200

    headings for reminder fields

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -125,11 +125,19 @@ const Task = ({ currentMatrix, text, completed, tasks, setTasks, id }) => {
               <Modal open={reminderModal}>
                 <div className='flex flex-col gap-3 mb-3'>
                   <h1 className='font-bold text-center text-2xl'>Set a reminder</h1>
+                  <h2> Date </h2>
                   <input 
                     className='border rounded-lg border-black p-1' 
                     type="datetime-local" 
                     value={reminderDateTime}
                     onChange={(e) => setReminderDateTime(e.target.value)}
+                  />
+                  <h2> Message </h2>
+                  <input 
+                    className='border rounded-lg border-black p-1' 
+                    type="text" 
+                    placeholder={text} 
+                    onChange={(e) => setReminderDescription(e.target.value)}
                   />
                   <div className='flex w-full justify-around'>
                     <button className='bg-cyan-500 text-md text-lg rounded-md px-4 h-12 text-white' onClick={() => saveReminder()}>Confirm</button>
