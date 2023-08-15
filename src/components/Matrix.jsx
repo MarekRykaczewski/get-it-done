6d3788a0767c63c0d6884ff3fe382643aebef484
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-08-15 12:55:31 +0200

    add task search

--- src/components/Matrix.jsx
+++ src/components/Matrix.jsx
@@ -5,6 +5,19 @@ import Section from "./Section"
 const Matrix = ({ currentMatrix }) => {
 
   const [tasks, setTasks] = useState([])
+  const [showSearchBar, setShowSearchBar] = useState(false)
+  const [searchQuery, setSearchQuery] = useState("")
+
+  const filteredTasks = tasks.filter((task) =>
+      task.name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+
+  const toggleSearchBar = () => {
+    setShowSearchBar(!showSearchBar);
+    if (showSearchBar) {
+      setSearchQuery(""); // Reset the search query when closing the search bar
+    }
+  };
 
   useEffect(() => {
     setTasks(JSON.parse(localStorage.getItem(currentMatrix.id)) || [])
@@ -20,15 +33,36 @@ const Matrix = ({ currentMatrix }) => {
   return (  
     <div className="flex flex-col items-center justify-center gap-5 py-5 px-10 h-full">
       <h1 className='text-3xl font-bold text-slate-500 max-w-[800px] drop-shadow'> {currentMatrix.name}</h1>
-      <CreateTask 
-        currentMatrix={currentMatrix}
-        setTasks={setTasks} 
-      />
+      
+      <div className="flex gap-4 items-center">
+        <CreateTask 
+          currentMatrix={currentMatrix}
+          setTasks={setTasks} 
+        />
+        <button
+        className={`${!showSearchBar ? "bg-blue-500" : "bg-red-500"} transition text-md text-lg rounded-md w-44 px-4 h-12 text-white`}
+        onClick={toggleSearchBar}
+        >
+        {!showSearchBar ? "Search Tasks" : "Close Search"}
+        </button>
+      </div>
+
+    {showSearchBar && (
+      <div className="fixed top-5 right-5 z-10">
+        <input
+          type="text"
+          placeholder="Search tasks..."
+          className="border-2 border-slate-400 text-lg bg-slate-100 rounded-md mr-4 pl-2 h-12 w-64 px-1"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+      </div>
+    )}
      <div className='grid grid-rows-2 grid-cols-2 h-full w-full aspect-square'>
                 
      {Object.keys(positionMap).map((category) => {
       const { col, row, color } = positionMap[category];
-      const sectionTasks = tasks.filter((task) => task.category === category);
+      const sectionTasks = filteredTasks.filter((task) => task.category === category);
 
       return (
         <Section
