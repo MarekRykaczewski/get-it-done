f8dcc8665b457873a05f0b1c7b77c09f020cb213
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-08-21 14:50:14 +0200

    use ProgressBar

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,6 +1,7 @@
 import Task from "./Task"
 import { useDrop } from "react-dnd"
 import { ItemTypes } from "./Task"
+import ProgressBar from "./ProgressBar"
 
 
 const Section = ({ currentMatrix, sectionTasks, tasks, setTasks, color, position, category }) => {
@@ -30,8 +31,10 @@ const Section = ({ currentMatrix, sectionTasks, tasks, setTasks, color, position
     })
   }
 
+  const completeTasks = sectionTasks.filter(task => task.completed == true)
+
   return (
-    <div ref={drop} className={`flex flex-col p-6 gap-2 items-center justify-start ${position} ${color} h-full w-full overflow-y-auto`}>
+    <div ref={drop} className={`relative flex flex-col p-6 gap-2 items-center justify-start ${position} ${color} h-full w-full overflow-y-auto`}>
     {sectionTasks.map(task => (
       <Task 
         currentMatrix={currentMatrix}
@@ -43,6 +46,11 @@ const Section = ({ currentMatrix, sectionTasks, tasks, setTasks, color, position
         completed={task.completed}
       /> 
     ))}
+    <div className="flex flex-row gap-3 w-full items-center justify-center absolute p-3 bottom-0 right-0">
+      <ProgressBar value={completeTasks.length} total={sectionTasks.length} />
+      <span className="flex-3 text-center"> {completeTasks.length} / {sectionTasks.length}</span>
+    </div>
+
   </div>
   )
 }
