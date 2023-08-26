dddfed0875067c93e2f5cf3231a4bb7afe759b1f
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-08-26 13:22:41 +0200

    apply darkMode to Modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,13 +1,16 @@
 import ReactDOM from 'react-dom'
+import { useSettings } from '../contexts/SettingsContext'
 
 const Modal = ({ open, children }) => {
+
+  const { settings } = useSettings()
 
   if (!open) return null
 
   return ReactDOM.createPortal (
     <>
     <div className='fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-70 z-49' />
-      <div className='fixed rounded-lg top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-10 z-50'>
+      <div className={`fixed rounded-lg top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ${settings.darkMode ? "bg-slate-800 text-white" : "bg-white text-slate-800"} transition-colors duration-500 p-10 z-50`}>
         {children}
       </div>
     </>,
